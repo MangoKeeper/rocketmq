9ecd78a5480d3f6336733c14449f1ed5c9bb57d0
Peter_SUM <withshenhui@gmail.com>
2018-11-21 13:18:42 +0800

    Initial commit

---
+++ src/main/java/com/peter/spring/boot/rocketmq/SpringBootRocketmqDemoApplication.java
@@ -0,0 +1,12 @@
+package com.peter.spring.boot.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRocketmqDemoApplication.class, args);
+	}
+}
